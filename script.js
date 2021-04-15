f9f6dacdcb0ee53d4a3d19c33938389b2daf8c4a
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-04-15 04:33:34 -0300

    Entrar funcionando, continuar conectado funcionando, atualizar msgs funcionando

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
-// Entrar na sala
-prompt("Qual seu nome?")
 
-// Pegando as msgs do servidor
-const requisicao = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages');
-requisicao.then(renderizarMensagens);
 
+//Variaveis globais
+let nomeChat="";
+// - - - - - -  - - - -  -
+
+enviarNome();
+// Ao entrar na sala, pergunto o nome e envio pro servidor
+function enviarNome() {
+    nomeChat = prompt("Qual seu nome?");
+    nomeChat = { name: nomeChat };
+    const requisicaoEnviar = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants', nomeChat);
+    requisicaoEnviar.then(exitoLogar);
+    requisicaoEnviar.catch(erroLogar);
+}
+
+//Trato as minhas respostas do servidor
+
+function exitoLogar(resposta) {
+    alert("conseguiu entrar");
+    pegaMensagensServidor();
+}
+
+function erroLogar(erro) {
+    enviarNome();
+}
+
+// Envia o nome do meu usuario, ja criado, para o servidor manter a conexão
+function manterConexao(){
+    console.log("continua logado");
+    const requisicaoEnviar = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status', nomeChat);
+}
+//Enviando a cada 5 segundos para manter conectado
+setInterval(manterConexao,5000);
+
+// Pega sa msgs do servidor
+function pegaMensagensServidor() {
+
+const requisicaoPegar = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages');
+requisicaoPegar.then(renderizarMensagens);
+
+}
 // Renderizando as msgs do servidor
 function renderizarMensagens(resposta){
-    console.log(resposta.data);
+    
     let mensagens = document.querySelector('.mensagens');
+
+    mensagens.innerHTML = "";
 
     for(let i = 0;i<resposta.data.length;i++){
         if(resposta.data[i].type === "message"){
@@ -27,7 +64,7 @@ function renderizarMensagens(resposta){
                 ${resposta.data[i].text}
             </div>
             `;
-        }else if(resposta.data[i].type=private_message){
+        }else if(resposta.data[i].type= "private_message"){ // Lembrar de tratar melhor esse caso
             mensagens.innerHTML += `
             <div class="mensagem privado">(${resposta.data[i].time})  
                 <b>${resposta.data[i].from}</b> para 
@@ -37,6 +74,7 @@ function renderizarMensagens(resposta){
             `;
         }
     }
-
+    console.log("estou atualizando");
 }
 
+setInterval(pegaMensagensServidor,3000);
